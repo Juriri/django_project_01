8782d17f2463e47792f2b53cf336226d0c45de47
HyunJu Lee <lhj7826@naver.com>
2025-07-29 11:20:20 +0900

    feat: update, delete form 추가

--- todolist/urls.py
+++ todolist/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("<int:todo_id>/", views.todo_info, name="todo_info"),
     path("create/", views.todo_create, name="todo_create"),
     path("<int:todo_id>/update/", views.todo_update, name="todo_update"),
-    path("<int:todo_id>/delete/", views.todo_deelte, name="todo_delete"),
+    path("<int:todo_id>/delete/", views.todo_delete, name="todo_delete"),
 ]
