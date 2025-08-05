738e19a8c2daa01e769d6a8879c045cd55e86ed4
HyunJu Lee <lhj7826@naver.com>
2025-08-05 11:00:24 +0900

    feat: 커스텀 로그인, 회원가입 html추가

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("bookmarks/", include("bookmark.urls")),
     # path("todo/", include("todolist.urls")),
     path("cbv/", include("todolist.urls")),
-    path("accounts/", include("users.urls")),
+    # path("accounts/", include("users.urls")),
+    path("users/", include("users.urls")),
     path("summernote/", include("django_summernote.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
+from .cb_views import SignupView, LoginView, verify_email
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('signup/', views.sign_up, name='signup'),
-path('logout/', views.logout, name='logout')
+    path("login/", LoginView.as_view(), name="login"),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("logout/", LogoutView.as_view(next_page="users:login"), name="logout"),
+    path("verify/", verify_email, name="verify_email"),
 ]
